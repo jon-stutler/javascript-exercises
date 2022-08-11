bb687b342208d2ee00d35ac221b3b6266b79f1d7
jon-stutler <82524410+jon-stutler@users.noreply.github.com>
2022-08-10 23:49:51 -0500

    complete exercises 08 and 09

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) { // This one works!
+  let total = 0;
+  for(let i in a) {
+    total += a[i];
+  };
+	return total;
 };
 
-const multiply = function() {
-
+const multiply = function(a) { // This one works now :)
+  let total = 1;
+  for(let i in a) {
+    total *= a[i];
+  };
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0 || a === 1)
+  return 1;
+  for (let i = a-1; i >= 1; i--) {
+    a *= i;
+  }
+  return a;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (a) {
+    let noSpaces = a.replace(/\s+/g, '');
+    let noPunct = noSpaces.replace(/[^\w\s\']|_/g, '');
+    let lowerCase = noPunct.toLowerCase();
+    let reversed = lowerCase.split('').reverse().join('');
 
+    if (reversed == lowerCase) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
