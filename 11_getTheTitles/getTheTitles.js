c402747c99081063dc944bdd8dffe98b474d547c
jon-stutler <82524410+jon-stutler@users.noreply.github.com>
2022-08-14 20:30:15 -0500

    complete exercises

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function() {
-
+  const getTheTitles = function(a) {
+    let titles = a.map(book => book.title);
+    return(titles);
 };
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
